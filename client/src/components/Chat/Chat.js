6a6e906646cd9f39e396ef6381bf908c29f1adbe
Nirma Perera <nirmaperera23@gmail.com>
2020-10-30 12:41:03 -0400

    added client and server files

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Chat = () => (
+
+    <div>
+        <h1>Chat</h1>
+    </div>
+
+
+
+)
+
+export default Chat;
---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState("");
+    return (
+        <div className="joinOuter_Containter">
+            <div>
+                <h1 className=""></h1>
+            </div>
+
+        </div>
+    )
+
+}
+
+
+export default Join;
