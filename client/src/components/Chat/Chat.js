058b62ad460e5fca6e483d421d3c86ac338e09ab
Nirma Perera <nirmaperera23@gmail.com>
2020-11-02 15:25:47 -0500

    added files for deployment

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000'
+    const ENDPOINT = 'https://chat-application23.herokuapp.com/'
 
 
 
