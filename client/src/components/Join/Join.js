e505119f12c5d84108de29df4ba4892b00388c2c
Nirma Perera <nirmaperera23@gmail.com>
2020-11-09 18:27:41 -0500

    redesign of login page

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import blueBubble from '../../images/circle-b.png'
+import greenBubble from '../../images/circle-g.png'
 
 import './Join.css';
 
@@ -9,6 +11,13 @@ const Join = () => {
 
     return (
         <div className="joinOuter_Containter">
+            <div className="bubbles-left ">
+                <img className="blue-bubble floating" src={blueBubble}></img>
+
+                <img className="green-bubble floating delay" src={greenBubble}></img>
+
+            </div>
+
             <div className="joinInner_Container">
                 <h1 className="heading"> Join </h1>
                 <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)}></input></div>
@@ -19,7 +28,21 @@ const Join = () => {
                 </Link>
             </div>
 
-        </div>
+            <div className="bubbles-top floating delay">
+                <img className="blue-bubble" src={blueBubble}></img>
+            </div>
+
+            <div className="bubbles-bottom floating">
+                <img className="blue-bubble" src={blueBubble}></img>
+            </div>
+
+            <div className="bubbles-right ">
+                <img className="blue-bubble floating " src={blueBubble}></img>
+                <img className="green-bubble floating delay" src={greenBubble}></img>
+            </div>
+
+
+        </div >
     )
 
 }
