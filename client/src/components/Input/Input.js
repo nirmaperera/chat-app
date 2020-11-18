83ae580572f77dfa55c0485dcc34a43f3b130e43
Nirma Perera <nirmaperera23@gmail.com>
2020-11-17 19:34:38 -0500

    show one media option at a time

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Input.css';
 
-const Input = ({ setMessage, sendMessage, message }) => {
+const Input = ({ setMessage, sendMessage, message, setTyping }) => {
+    let timer, timeoutVal = 1000;
+
+
+    // const handleKeyPress = (e) => {
+    //     if (e.key === 'Enter') {
+    //         sendMessage(e)
+    //     } else {
+    //         window.clearTimeout(timer)
+    //         setTyping(true);
+    //     }
+    //     //event => event.key === 'Enter' ? sendMessage(event) : null
+    // }
+    // const handleKeyUp = (e) => {
+    //     window.clearTimeout(timer);
+    //     timer = window.setTimeout(() => {
+    //         setTyping(false);
+    //     }, timeoutVal);
+    // }
+
     return (
         <form className="form">
             <input
@@ -11,7 +30,8 @@ const Input = ({ setMessage, sendMessage, message }) => {
                 placeholder="Type a message..."
                 value={message}
                 onChange={({ target: { value } }) => setMessage(value)}
-                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+            // onKeyPress={(e) => handleKeyPress(e)}
+            // onKeyUp={(e) => handleKeyUp(e)}
             />
             <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
         </form>
--- client/src/components/InputMenu/InputMenu.js
+++ client/src/components/InputMenu/InputMenu.js
@@ -13,31 +13,46 @@ const InputMenu = ({ setMessage }) => {
     const [clickGif, setClickGif] = useState(false);
     const [clickImg, setClickImg] = useState(false);
     const [clickStickers, setClickStickers] = useState(false);
+
+    const gif = () => {
+        setClickGif(true);
+        setClickImg(false);
+        setClickStickers(false)
+    }
+
+    const img = () => {
+        setClickImg(true);
+        setClickGif(false);
+        setClickStickers(false);
+    }
+
+    const stickers = () => {
+        setClickImg(false);
+        setClickGif(false);
+        setClickStickers(true);
+    }
+
     return (
         <div className="inputMenu__container">
             <div className="inputMenu">
                 <div>
-                    <GifIcon onClick={() => setClickGif(true)} className="Icon" />
-                    <ImageSearchIcon onClick={() => setClickImg(true)} className="Icon" />
-                    <img src={sticker} width='48px' className="Icon" alt="stickers" onClick={() => setClickStickers(true)} />
+                    <GifIcon onClick={() => gif()} className="Icon" />
+                    <ImageSearchIcon onClick={() => img()} className="Icon" />
+                    <img src={sticker} width='48px' className="Icon" alt="stickers" onClick={() => stickers()} />
                 </div>
                 <div>
-                    {clickGif ? <CloseIcon className="close" onClick={() => setClickGif(false)} /> : null}
-                    {clickImg ? <CloseIcon className="close" onClick={() => setClickImg(false)} /> : null}
-                    {clickStickers ? <CloseIcon className="close" onClick={() => setClickStickers(false)} /> : null}
-
+                    {clickGif ? <CloseIcon className="close" onClick={() => setClickGif(false)} /> :
+                        clickImg ? <CloseIcon className="close" onClick={() => setClickImg(false)} /> :
+                            clickStickers ? <CloseIcon className="close" onClick={() => setClickStickers(false)} /> : null}
                 </div>
             </div>
 
             <div>
-                {clickGif ? <Gif setMessage={setMessage} library={"gifs"} setClickGif={setClickGif} /> : null}
-                {clickStickers ? <Gif setMessage={setMessage} library={"stickers"} setClickStickers={setClickStickers} /> : null}
-                {clickImg ? <ImageInput setMessage={setMessage} setClickImg={setClickImg} /> : null}
+                {clickGif ? <Gif setMessage={setMessage} library={"gifs"} setClickGif={setClickGif} /> :
+                    clickStickers ? <Gif setMessage={setMessage} library={"stickers"} setClickStickers={setClickStickers} /> :
+                        clickImg ? <ImageInput setMessage={setMessage} setClickImg={setClickImg} /> : null}
             </div>
-
-
         </div>
-
     )
 }
 
